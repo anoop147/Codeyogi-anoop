b79301b016084d0dd6f4368555dd5d229258cac1
saurabhgiri <saurabhgiri877@gmail.com>
2022-11-29 13:12:30 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="bg-green-400 h-screen text-center flex flex-col justify-center text-2xl">
+      Hello
+    </div>
+  );
+}
+
+export default App;

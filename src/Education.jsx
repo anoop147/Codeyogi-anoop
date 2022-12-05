6ee320e5d0fc2627a1929610ef79a48ecb6ababf
anoop <anupsingh.147@gmail.com>
2022-12-05 06:15:42 +0530

    initial commit

--- src/Education.jsx
+++ src/Education.jsx
@@ -4,7 +4,55 @@ import React from "react";
 function Education() {
   return (
     <div>
-        <div> 
+        <div className="m-8 font-serif "> 
+          <div className="m-4 h-80% ">
+                <div className="m-4 text-2xl text-gray-600 text-center"><h1 className="P-4">EDUCATIONAL QUALIFICATION</h1></div>
+                <div className="border-solid border-2 border-gray-300 text-gray-700">
+                    <div className="m-4 text-2xl text-center "> <h1 className="P-4">GRADUATION DEGREE</h1></div>
+                    <div className="m-4 flex  ">
+                      <div className="m-2">
+                        <div className="text-gray-600"  >Course & Branch Name</div>
+                        <div > Collage Name</div>
+                        <div > University Name</div>
+                      </div>
+                      <div className="m-2">
+                        <div>B.TECH with Information Technology </div>
+                        <div>Collage of Technology Pantnagar </div>
+                        <div>Govind Ballabh Pant University of Aggriculture and Technology Pantnagar </div>
+                      </div>
+                    </div>
+                </div>
+                <div className="border-solid border-2 border-gray-300 mt-4" >
+                    <div className="m-4 text-2xl text-center"> <h1 className="P-4">Diploma</h1></div>
+                    <div className="m-4 flex  ">
+                      <div className="m-2">
+                        <div >Course & Branch Name</div>
+                        <div > Collage Name</div>
+                        <div > University/Board Name</div>
+                      </div>
+                      <div className="m-2">
+                        <div>Diploma (Ploytechnic) with Information Technology </div>
+                        <div>Govt. Polytechnic Srinagar Garhwal </div>
+                        <div>Uttrakhabd Board of Technical Education </div>
+                      </div>
+                    </div>
+                </div>
+                <div className="border-solid border-2 border-gray-300 mt-4" >
+                    <div className="m-4 text-2xl text-center"> <h1 className="P-4">Schooling</h1></div>
+                    <div className="m-4 flex  ">
+                      <div className="m-2">
+                        <div >Passing Exam</div>
+                        <div > School Name</div>
+                        <div > University/Board Name</div>
+                      </div>
+                      <div className="m-2">
+                        <div>Heigh School </div>
+                        <div>Asha Ram Vedik Inter Collage Vikasnagar, Dehradun </div>
+                        <div> Board of School Education Uttrakhabd </div>
+                      </div>
+                    </div>
+                </div>
+          </div>
             
         </div>
     </div>
